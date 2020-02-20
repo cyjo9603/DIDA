1b6d2276a9dc0ae12ae52109aa8dbedaab6cb30d
조찬영 <cyjo9603@naver.com>
2020-02-20 15:24:03 +0900

    [feat] styled theme 객체 생성

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+  mainText: '#111111',
+  inputBorder: '#adb5bd',
+  notifyMessage_01: '#868e96',
+  notifyMessage_02: '#ced4da',
+  errorMessage: '#ff6679',
+
+  buttonBackground: '#203f9b',
+};
+
+export interface ITheme {
+  mainText: string;
+  inputBorder: string;
+  notifyMessage_01: string;
+  notifyMessage_02: string;
+  errorMessage: string;
+}
+
+export default theme;
