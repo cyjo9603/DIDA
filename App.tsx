26cad9f491f3757d89b38938d39e711df046d8bf
조찬영 <cyjo9603@naver.com>
2020-02-20 22:05:39 +0900

    [test] 화면 테스트용 코드 및 주석 처리

--- App.tsx
+++ App.tsx
@@ -5,12 +5,18 @@ import {ThemeProvider} from 'styled-components';
 import theme from './src/theme';
 
 import CodeExchange from './src/CodeExchange';
+import SelectDate from './src/SelectDate';
+import SelectColor from './src/SelectColor';
 
 const App = () => {
   return (
     <>
       <StatusBar backgroundColor="white" barStyle="dark-content"></StatusBar>
-      <ThemeProvider theme={theme}></ThemeProvider>
+      <ThemeProvider theme={theme}>
+        {/* <CodeExchange /> */}
+        {/* <SelectDate /> */}
+        <SelectColor />
+      </ThemeProvider>
     </>
   );
 };
