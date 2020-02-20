e046b98e61e8b2cc85e2b71497472088d3d0f9ca
조찬영 <cyjo9603@naver.com>
2020-02-20 16:08:36 +0900

    [feat] BottomButton Component 작성

---
+++ src/component/BottomButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {TouchableOpacity, Text} from 'react-native';
+import styled from 'styled-components/native';
+
+import {ITheme} from '../theme';
+
+const BottomButton = () => {
+  return (
+    <MoveButton
+      onPress={() => {
+        console.log('bottom');
+      }}>
+      <ButtonText>다음</ButtonText>
+    </MoveButton>
+  );
+};
+
+const MoveButton = styled.TouchableOpacity<{theme: ITheme}>`
+  width: 100%;
+  height: 66.5333px;
+  background-color: ${props => props.theme.buttonBackground};
+  justify-content: center;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-weight: 700;
+  font-size: 20px;
+  text-align: center;
+`;
+
+export default BottomButton;
