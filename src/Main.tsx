e36d59de80efae3d09852178bcf6209eec5f0dfc
조찬영 <cyjo9603@naver.com>
2020-02-21 16:00:47 +0900

    [feat] Header 추가 및 tab 이동 기능 구현

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components/native';
 
 import {ITheme} from './theme';
 
+import Header from './component/Header';
 import Container from './component/Container';
 import MainTabItem from './component/MainTabItem';
 import MainToday from './MainToday';
@@ -11,11 +12,14 @@ import MainToday from './MainToday';
 const Main = () => {
   const [state, setState] = useState('TODAY');
 
+  const navTouch = (NavTitle: string) => setState(NavTitle);
+
   return (
     <Container>
+      <Header />
       <Nav>
-        <MainTabItem title="TODAY" state={state} />
-        <MainTabItem title="DIARY" state={state} />
+        <MainTabItem title="TODAY" state={state} press={navTouch} />
+        <MainTabItem title="DIARY" state={state} press={navTouch} />
       </Nav>
       {state === 'TODAY' ? <MainToday /> : <></>}
     </Container>
