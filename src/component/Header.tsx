876962e2db3baa31954c8d144e843bd93f41caa3
조찬영 <cyjo9603@naver.com>
2020-02-21 16:00:18 +0900

    [feat] Header Component 구현

---
+++ src/component/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, TouchableOpacity, Image} from 'react-native';
+import styled from 'styled-components/native';
+
+const Header = () => {
+  return (
+    <Container>
+      <TouchableOpacity>
+        <Image
+          source={require('../../image/drawable-xxxhdpi/ic_drawer.png')}
+          style={{width: 48, height: 48}}></Image>
+      </TouchableOpacity>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex-direction: row;
+  width: 100%;
+  height: 64px;
+  padding: 8px 10.6666px;
+`;
+
+export default Header;
