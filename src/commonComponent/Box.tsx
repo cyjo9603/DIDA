48fb6af9e1165bcffb16de2278155a844f6bda65
조찬영 <cyjo9603@naver.com>
2020-02-22 20:47:37 +0900

    [hotfix] 인자로 받는 객체속성 명 변경

--- src/commonComponent/Box.tsx
+++ src/commonComponent/Box.tsx
@@ -3,15 +3,15 @@ import {View} from 'react-native';
 import styled from 'styled-components/native';
 
 const Box = styled.View<{
-  top?: number;
-  bottom?: number;
-  left?: number;
-  right?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
 }>`
-  margin-top: ${props => (props.top ? props.top : 0)}px;
-  margin-bottom: ${props => (props.bottom ? props.bottom : 0)}px;
-  margin-left: ${props => (props.left ? props.left : 0)}px;
-  margin-right: ${props => (props.right ? props.right : 0)}px;
+  margin-top: ${props => (props.marginTop ? props.marginTop : '0')}px;
+  margin-bottom: ${props => (props.marginBottom ? props.marginBottom : '0')}px;
+  margin-left: ${props => (props.marginLeft ? props.marginLeft : '0')}px;
+  margin-right: ${props => (props.marginRight ? props.marginRight : '0')}px;
 `;
 
 export default Box;
