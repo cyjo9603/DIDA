afffa0dd864c0c0d556c62baff15c6fa01b4c5bd
조찬영 <cyjo9603@naver.com>
2020-03-01 21:07:29 +0900

    [feat] reduce link & navigation header setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {createStore} from 'redux';
+import {Provider, useSelector} from 'react-redux';
 import {StatusBar} from 'react-native';
 import {ThemeProvider} from 'styled-components';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, TransitionSpecs, HeaderStyleInterpolators} from '@react-navigation/stack';
 
 import theme from './theme';
 
@@ -11,6 +13,8 @@ import CodeExchange from './page/CodeExchange';
 import SelectDate from './page/SelectDate';
 import SelectColor from './page/SelectColor';
 import Main from './page/Main';
+import StackHeader from './component/StackHeader';
+import rootReducer, {IRootState} from './reducers/index';
 
 export type StackParamList = {
   CodeExchange: undefined;
@@ -20,23 +24,50 @@ export type StackParamList = {
 
 const Stack = createStackNavigator<StackParamList>();
 
+const store = createStore(rootReducer);
+
 const App = () => {
+  const {isConnected} = useSelector((state: IRootState) => state.userReducer);
+
   return (
     <>
       <StatusBar backgroundColor="white" barStyle="dark-content"></StatusBar>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="CodeExchange">
-            <Stack.Screen name="CodeExchange" component={CodeExchange} />
-            <Stack.Screen name="SelectDate" component={SelectDate} />
-            <Stack.Screen name="SelectColor" component={SelectColor} />
-          </Stack.Navigator>
-        </NavigationContainer>
-        {/* <CodeExchange /> */}
-        {/* <SelectDate /> */}
-        {/* <SelectColor /> */}
-        {/* <Main /> */}
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          {isConnected ? (
+            <NavigationContainer>
+              <Stack.Navigator initialRouteName="CodeExchange">
+                <Stack.Screen
+                  name="CodeExchange"
+                  component={CodeExchange}
+                  options={{
+                    title: '',
+                    header: () => <StackHeader />,
+                  }}
+                />
+                <Stack.Screen
+                  name="SelectDate"
+                  component={SelectDate}
+                  options={{
+                    title: '',
+                    header: ({navigation}) => <StackHeader goBack={navigation.goBack} />,
+                  }}
+                />
+                <Stack.Screen
+                  name="SelectColor"
+                  component={SelectColor}
+                  options={{
+                    title: '',
+                    header: ({navigation}) => <StackHeader goBack={navigation.goBack} />,
+                  }}
+                />
+              </Stack.Navigator>
+            </NavigationContainer>
+          ) : (
+            <Main />
+          )}
+        </ThemeProvider>
+      </Provider>
     </>
   );
 };
