f20465e437697ed607e93e38b8a1f09a4f7e0146
조찬영 <cyjo9603@naver.com>
2020-03-05 23:09:02 +0900

    [feat] backButton component optimization

--- src/component/BackButton.tsx
+++ src/component/BackButton.tsx
@@ -1,20 +1,25 @@
-import React, {FunctionComponent} from 'react';
-import {TouchableOpacity, Image} from 'react-native';
+import React, {FunctionComponent, useMemo, memo} from 'react';
+import {TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 
 interface IProps {
   onPress: () => void;
 }
 
-const BackButton: FunctionComponent<IProps> = ({onPress}) => (
-  <TouchableOpacity onPress={onPress}>
-    <Back source={require('../../image/drawable-xxxhdpi/ic_back.png')} />
-  </TouchableOpacity>
-);
+const BackButton: FunctionComponent<IProps> = ({onPress}) => {
+  const imgSource = useMemo(() => require('../../image/drawable-xxxhdpi/ic_back.png'), []);
+  const onPressFunc = useMemo(() => onPress, [onPress]);
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Back source={imgSource} />
+    </TouchableOpacity>
+  );
+};
 
 const Back = styled.Image`
   width: 48px;
   height: 48px;
 `;
 
-export default BackButton;
+export default memo(BackButton);
