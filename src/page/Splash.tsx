682a4925292c6120d1a2fc96745fd954e045ed5c
Chanyeong Cho <cyjo9603@naver.com>
2020-03-20 22:01:39 +0900

    [refactor] refactor get local data

--- src/page/Splash.tsx
+++ src/page/Splash.tsx
@@ -1,12 +1,10 @@
 import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import {getLoaclData} from '../reducers/user/getLocalData';
-import {userInfoCheckRequest} from '../reducers/user/userInfo';
-import {setIsConnected} from '../reducers/user/setIsConnected';
 
-import {USER_KEY, PARTNER_KEY} from '../storageKey';
+import {USER_KEY} from '../storageKey';
 
 const Splash = () => {
   const dispatch = useDispatch();
@@ -15,20 +13,7 @@ const Splash = () => {
     (async function() {
       try {
         const userCode = await AsyncStorage.getItem(USER_KEY);
-        const partnerCode = await AsyncStorage.getItem(PARTNER_KEY);
-        if (userCode || partnerCode) {
-          console.log('true');
-          console.log(userCode);
-          const data = {
-            userCode: userCode,
-            partnerCode: partnerCode,
-          };
-          dispatch(getLoaclData(data));
-          partnerCode ? dispatch(setIsConnected(true)) : dispatch(setIsConnected(false));
-        } else {
-          console.log('null');
-          dispatch(setIsConnected(false));
-        }
+        dispatch(getLoaclData(userCode));
       } catch (e) {
         console.log('false');
       }
