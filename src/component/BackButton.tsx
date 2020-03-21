82d5c3b1b82e2954ee01ee58234b80da470ddbf9
Chanyeong Cho <cyjo9603@naver.com>
2020-03-21 16:25:46 +0900

    [feat] extend back button

--- src/component/BackButton.tsx
+++ src/component/BackButton.tsx
@@ -2,17 +2,25 @@ import React, {FunctionComponent, useMemo, memo} from 'react';
 import {TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 
+import {ThemeType} from '../theme';
+
 interface IProps {
   onPress: () => void;
+  type: 'back' | 'exit' | 'submit';
 }
 
-const BackButton: FunctionComponent<IProps> = ({onPress}) => {
-  const imgSource = useMemo(() => require('../../image/drawable-xxxhdpi/ic_back.png'), []);
+const BackButton: FunctionComponent<IProps> = ({onPress, type}) => {
+  const imageSrc = useMemo(() => {
+    if (type === 'back') {
+      return require('../../image/drawable-xxxhdpi/ic_back.png');
+    }
+    return require('../../image/drawable-xxxhdpi/ic_close.png');
+  }, []);
   const onPressFunc = useMemo(() => onPress, [onPress]);
 
   return (
     <TouchableOpacity onPress={onPressFunc}>
-      <Back source={imgSource} />
+      {type === 'submit' ? <SubmitText>게시</SubmitText> : <Back source={imageSrc} />}
     </TouchableOpacity>
   );
 };
@@ -20,6 +28,12 @@ const BackButton: FunctionComponent<IProps> = ({onPress}) => {
 const Back = styled.Image`
   width: 48px;
   height: 48px;
+`;
+
+const SubmitText = styled.Text<{theme: ThemeType}>`
+  font-family: ${props => props.theme.Font.EB};
+  font-size: 20px;
+  color: ${props => props.theme.itemColor.navy_01};
 `;
 
 export default memo(BackButton);
